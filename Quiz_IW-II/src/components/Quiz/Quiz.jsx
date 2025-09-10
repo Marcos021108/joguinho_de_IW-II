150435e08d4d2d1930e735047e59d597122a869c
Marcos <161627757+Marcos021108@users.noreply.github.com>
2025-09-10 14:58:44 -0300

    .......................................

---
+++ Quiz_IW-II/src/components/Quiz/Quiz.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () =>{
+  return (
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>blablablabla?</h2>
+        <ul>
+            <li>blabla?</li>
+            <li>blablabla?</li>
+            <li>bla?</li>
+            <li>blablablablablabla?</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 de 5 perguntas</div>
+    </div>
+  )
+}
+
+export default Quiz
